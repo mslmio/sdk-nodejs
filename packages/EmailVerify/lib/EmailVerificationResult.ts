4a41f93033f5b4fcf4760659591fd21cd2cc53d9
Muhammad Ahmad <118795373+muhammadahmadmslm@users.noreply.github.com>
2024-01-09 19:17:09 +0500

    readme added

--- packages/EmailVerify/lib/EmailVerificationResult.ts
+++ packages/EmailVerify/lib/EmailVerificationResult.ts
@@ -2,78 +2,78 @@
  * Interface representing the result of an email verification.
  */
 export interface EmailVerificationResult {
-  /**
-   * The verified email address.
-   */
-  email: string;
+    /**
+     * The verified email address.
+     */
+    email: string;
 
-  /**
-   * The username portion of the email address.
-   */
-  username: string;
+    /**
+     * The username portion of the email address.
+     */
+    username: string;
 
-  /**
-   * The domain of the email address.
-   */
-  domain: string;
+    /**
+     * The domain of the email address.
+     */
+    domain: string;
 
-  /**
-   * Indicates whether the email address is malformed.
-   */
-  malformed: boolean;
+    /**
+     * Indicates whether the email address is malformed.
+     */
+    malformed: boolean;
 
-  /**
-   * A suggestion for a valid email address if the provided one is malformed.
-   */
-  suggestion: string;
+    /**
+     * A suggestion for a valid email address if the provided one is malformed.
+     */
+    suggestion: string;
 
-  /**
-   * The status of the email verification.
-   */
-  status: string;
+    /**
+     * The status of the email verification.
+     */
+    status: string;
 
-  /**
-   * Indicates whether the email address has a mailbox.
-   */
-  has_mailbox: boolean;
+    /**
+     * Indicates whether the email address has a mailbox.
+     */
+    has_mailbox: boolean;
 
-  /**
-   * Indicates whether the email address accepts all incoming emails.
-   */
-  accept_all: boolean;
+    /**
+     * Indicates whether the email address accepts all incoming emails.
+     */
+    accept_all: boolean;
 
-  /**
-   * Indicates whether the email address is disposable.
-   */
-  disposable: boolean;
+    /**
+     * Indicates whether the email address is disposable.
+     */
+    disposable: boolean;
 
-  /**
-   * Indicates whether the email address is a free email address.
-   */
-  free: boolean;
+    /**
+     * Indicates whether the email address is a free email address.
+     */
+    free: boolean;
 
-  /**
-   * Indicates whether the email address is associated with a role or position.
-   */
-  role: boolean;
+    /**
+     * Indicates whether the email address is associated with a role or position.
+     */
+    role: boolean;
 
-  /**
-   * An array of mail exchanges (MX) associated with the domain.
-   */
-  mx: MailExchange[];
+    /**
+     * An array of mail exchanges (MX) associated with the domain.
+     */
+    mx: MailExchange[];
 }
 
 /**
  * Interface representing a Mail Exchange (MX) record.
  */
 export interface MailExchange {
-  /**
-   * The host or domain of the mail exchange.
-   */
-  host: string;
+    /**
+     * The host or domain of the mail exchange.
+     */
+    host: string;
 
-  /**
-   * The preference value for the mail exchange.
-   */
-  pref: number;
+    /**
+     * The preference value for the mail exchange.
+     */
+    pref: number;
 }
--- packages/OTP/README.md
+++ packages/OTP/README.md
@@ -1,11 +1,24 @@
 # `OTP`
 
-> TODO: description
+The official MSLM Node.js SDK for handling One-Time Password (OTP)
+functionality.
+
+[![npm version](https://badge.fury.io/js/%40mslm%2Fotp.svg)](https://badge.fury.io/js/%40mslm%2Fotp)
+[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)
+[![GitHub issues](https://img.shields.io/github/issues/mslmio/sdk-nodejs)](https://github.com/mslmio/sdk-nodejs/issues)
+
+## Installation
+
+**npm**
+
+```bash
+npm install @mslm/otp
+```
+
+**yarn**
+
+```bash
+yarn add @mslm/otp
+```
 
 ## Usage
-
-```
-const otp = require('OTP');
-
-// TODO: DEMONSTRATE API
-```
